45c540703ea56d1f5616fcce24b0007b6621bc98
Jeremy, Yen <r00b46024@ntu.edu.tw>
2016-04-24 17:41:35 -0400

    Revert "revise error message"
    This reverts commit d18960c904508b450e436f61724e8f1ce234f171.

--- App/src/pycorn.py
+++ App/src/pycorn.py
@@ -11,7 +11,7 @@ import sys
 
 
 if len(sys.argv) != 4:
-	print "Please provide correct parameters: <inputfile> <outpufile> <windowsize>"
+	print "Please provide correct parameters"
 	sys.exit()
 
 try:
@@ -19,7 +19,7 @@ try:
 	outfile=sys.argv[2]
 	windowSlideSize = int(sys.argv[3])
 except:
-	print "Please provide correct parameters: <inputfile> <outpufile> <windowsize>"
+	print "Please provide correct parameters"
 
 modelfile = "pipelineJZ.pkl"
 seqfile=inputFile
